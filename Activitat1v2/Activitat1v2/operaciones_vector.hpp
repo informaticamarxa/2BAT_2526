d8910bbbe79fa256281fc4489310e6b7617c51f6
informaticamarxa <informatica@iesmarxadella.org>
2025-12-01 19:03:12 +0100

    Afegeix Activitat1v2 amb fitxers i carpetes

---
+++ Activitat1v2/Activitat1v2/operaciones_vector.hpp
@@ -0,0 +1,11 @@
+#ifndef OPERACIONES_VECTOR_HPP
+#define OPERACIONES_VECTOR_HPP
+
+#include <vector>
+
+int SumaImpares(const std::vector<int> &v);
+int ProductoPares(const std::vector<int> &v);
+int Divisibles3(const std::vector<int> &v, std::vector<int> &vResultado);
+void ImprimirVector(const std::vector<int> &v);
+
+#endif
